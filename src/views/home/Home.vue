a99b74c14a990efb6fb15a0ced634e58f01c43bd
Carey-cy <changanazai@163.com>
2020-06-03 22:35:29 +0800

    首页 请求数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,11 +6,33 @@
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
-  
+  import {getHomeMultidata} from 'network/home.js'
+
   export default {
     name: 'Home',
     components: {
       NavBar,
+    },
+    data() {
+      return {
+        banners:[],
+        recommends:[]
+        // dkeyword:[],
+        // keywords:[]
+        // result:null
+      }
+    },
+    created(){
+      //请求多个数据
+      getHomeMultidata().then(res=>{    
+        // this.result = res
+        // console.log(res);
+        
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list
+        // this.dkeyword = res.data.dkeyword.list
+        // this.keywords = res.data.keywords.list
+      })
     }
   }
 </script>
