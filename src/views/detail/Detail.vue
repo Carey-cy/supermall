63a18943d180f0c2945f2747e4dccada3c74da41
Carey-cy <changanazai@163.com>
2020-06-24 10:17:32 +0800

    根据id请求数据

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -7,11 +7,26 @@
 <script>
   import DetailNav from './childComps/DetailNav';
 
+  import {getDetail} from 'network/detail.js';
+
   export default {
     name:'Detail',
+    data() {
+      return {
+        iid:null
+      }
+    },
     components: {
       DetailNav
-    }
+    },
+    created() {
+      //1.save iid
+      this.iid = this.$route.params.iid
+      //2.network request
+      getDetail(this.iid).then(res =>{
+        console.log(res)
+      })
+    },
   }
 </script>
 
