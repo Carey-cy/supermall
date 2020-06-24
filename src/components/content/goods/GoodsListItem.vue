c811e706a523a62982aa1660c4a8ac5186d4e488
Carey-cy <changanazai@163.com>
2020-06-24 10:00:53 +0800

    点击商品跳转详情+navbar封装

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -32,51 +32,51 @@
 </script>
 
 <style>
-  .goods-item {
-    position: relative;
-    padding-bottom: 40px;
+.goods-item {
+  position: relative;
+  padding-bottom: 40px;
 
-    width: 48%;
-  }
+  width: 48%;
+}
 
-  .goods-item img {
-    width: 100%;
-    border-radius: 5px;
-  }
+.goods-item img {
+  width: 100%;
+  border-radius: 5px;
+}
 
-  .goods-info {
-    font-size: 12px;
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 5px;
-    text-align: center;
-    overflow: hidden;
-  }
+.goods-info {
+  font-size: 12px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 5px;
+  text-align: center;
+  overflow: hidden;
+}
 
-  .goods-info p {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    margin-bottom: 3px;
-  }
+.goods-info p {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-bottom: 3px;
+}
 
-  .goods-info .price {
-    color: var(--color-high-text);
-    margin-right: 20px;
-  }
+.goods-info .price {
+  color: var(--color-high-text);
+  margin-right: 20px;
+}
 
-  .goods-info .cfav {
-    position: relative;
-  }
+.goods-info .cfav {
+  position: relative;
+}
 
-  .goods-info .cfav::before {
-    content:'';
-    position: absolute;
-    left: -15px;
-    top:-1px;
-    width: 14px;
-    height: 14px;
-    background: url(~assets/img/common/collect.svg) 0 0/14px 14px ;
-  }
+.goods-info .cfav::before {
+  content: "";
+  position: absolute;
+  left: -15px;
+  top: -1px;
+  width: 14px;
+  height: 14px;
+  background: url(~assets/img/common/collect.svg) 0 0/14px 14px;
+}
 </style>
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,14 +1,20 @@
 <template>
-  <div>
-
+  <div id="detail">
+    <detail-nav/>
   </div>
 </template>
 
 <script>
+  import DetailNav from './childComps/DetailNav';
+
   export default {
-    name:'Detail'
+    name:'Detail',
+    components: {
+      DetailNav
+    }
   }
 </script>
 
 <style scoped>
+
 </style>
