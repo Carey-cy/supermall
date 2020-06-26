f6bcfe0bb02f376b17f2b6124b1612021383577f
Carey-cy <changanazai@163.com>
2020-06-26 18:18:11 +0800

    详情页-商品基础信息+店铺信息展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,34 +2,49 @@
   <div id="detail">
     <detail-nav/>
     <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods = "goods" />
+    <detail-shop-info :shop="shop" />
   </div>
 </template>
 
 <script>
   import DetailNav from './childComps/DetailNav';
   import DetailSwiper from './childComps/DetailSwiper';
+  import DetailBaseInfo from './childComps/DetailBaseInfo';
+  import DetailShopInfo from './childComps/DetailShopInfo';
 
-  import {getDetail} from 'network/detail.js';
+  import {getDetail,Goods,Shop} from 'network/detail.js';
 
   export default {
     name:'Detail',
     data() {
       return {
         iid:null,
-        topImages:[]
+        topImages:[],
+        goods:{},
+        shop: {}
       }
     },
     components: {
       DetailNav,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo,
+      DetailShopInfo
     },
     created() {
+      const data = 
       //1.save iid
       this.iid = this.$route.params.iid
       //2.network request
       getDetail(this.iid).then(res =>{
         console.log(res);
-        this.topImages = res.data.result.itemInfo.topImages
+        const data = res.data.result
+        //1.获取顶部轮播图的图片数据
+        this.topImages = data.itemInfo.topImages
+        //2.获取商品数据
+        this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+        //3.获取店铺信息
+        this.shop = new Shop(data.shopInfo)
       })
     },
   }
